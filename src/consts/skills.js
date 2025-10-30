3ddce25768283d593622d08aed15d7ece83832b2
Mateusz Marek <marek.mateusz@protonmail.com>
2025-10-30 17:30:04 +0100

    Update skills icons

--- src/consts/skills.js
+++ src/consts/skills.js
@@ -1,60 +1,34 @@
+const ICON_BASE_URL = "https://skillicons.dev/icons?i=";
+
 export const skills = [
-  {
-    image: "https://cdn-icons-png.flaticon.com/512/1216/1216733.png",
-    text: "HTML",
-  },
-  {
-    image: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
-    text: "CSS",
-  },
-  {
-    image: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
-    text: "JavaScript",
-  },
-  {
-    image:
-      "https://static-00.iconduck.com/assets.00/typescript-plain-icon-256x256-ypojgpyj.png",
-    text: "TypeScript",
-  },
-  {
-    image: "https://cdn-icons-png.flaticon.com/512/1126/1126012.png",
-    text: "React",
-  },
-  {
-    image: "https://cdn-icons-png.flaticon.com/512/919/919830.png",
-    text: "PHP",
-  },
-  {
-    image:
-      "https://static-00.iconduck.com/assets.00/laravel-icon-1990x2048-xawylrh0.png",
-    text: "Laravel",
-  },
-  {
-    image: "https://static-00.iconduck.com/assets.00/symfony-icon-512x512-94lugehx.png",
-    text: "Symfony",
-  },
-  {
-    image: "https://cdn-icons-png.flaticon.com/512/6132/6132221.png",
-    text: "C#",
-  },
-  {
-    image: "https://static-00.iconduck.com/assets.00/java-icon-378x512-w60vlu77.png",
-    text: "Java",
-  },
-  {
-    image: "https://static-00.iconduck.com/assets.00/file-type-mysql-icon-512x504-otlvcwde.png",
-    text: "MySQL "
-  },
-  {
-    image: "https://static-00.iconduck.com/assets.00/git-icon-512x512-61zfmvxk.png",
-    text: "Git",
-  },
-  {
-    image: "https://static-00.iconduck.com/assets.00/google-cloud-icon-512x412-8rnz6wkz.png",
-    text: "Google Cloud",
-  },
-  {
-    image: "https://static-00.iconduck.com/assets.00/linux-icon-439x512-rnhe78x0.png",
-    text: "Linux",
-  },
+  // Frontend fundamentals
+  { image: `${ICON_BASE_URL}html`, text: "HTML" },
+  { image: `${ICON_BASE_URL}css`, text: "CSS" },
+  { image: `${ICON_BASE_URL}js`, text: "JavaScript" },
+  { image: `${ICON_BASE_URL}ts`, text: "TypeScript" },
+
+  // Frontend frameworks and styling
+  { image: `${ICON_BASE_URL}react`, text: "React" },
+  { image: `${ICON_BASE_URL}nextjs`, text: "Next.js" },
+  { image: `${ICON_BASE_URL}tailwind`, text: "TailwindCSS" },
+
+  // Backend languages and frameworks
+  { image: `${ICON_BASE_URL}php`, text: "PHP" },
+  { image: `${ICON_BASE_URL}laravel`, text: "Laravel" },
+  { image: `${ICON_BASE_URL}symfony`, text: "Symfony" },
+  { image: `${ICON_BASE_URL}nodejs`, text: "Node.js" },
+
+  // Additional programming languages
+  { image: `${ICON_BASE_URL}java`, text: "Java" },
+  { image: `${ICON_BASE_URL}cs`, text: "C#" },
+
+  // Databases
+  { image: `${ICON_BASE_URL}mysql`, text: "MySQL" },
+  { image: `${ICON_BASE_URL}mongodb`, text: "MongoDB" },
+
+  // Tools and platforms
+  { image: `${ICON_BASE_URL}git`, text: "Git" },
+  { image: `${ICON_BASE_URL}linux`, text: "Linux" },
+  { image: `${ICON_BASE_URL}docker`, text: "Docker" },
+  { image: `${ICON_BASE_URL}gcp`, text: "Google Cloud" },
 ];
